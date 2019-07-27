1775491b2d4f5ba0dc552d6302c98960c3c0a580
hesham tatawy <heshamtatawy@gmail.com>
2019-07-28 00:30:42 +0300

    Initial Commit

---
+++ shearie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  shearie
+//
+//  Created by hesham on 25/11/1440 AH.
+//  Copyright © 1440 hesham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
